35c0bdf3b1bf9ef913f600794bf85f4023f4870b
DRIVANnega <43270243+DRIVANnega@users.noreply.github.com>
2026-05-20 12:14:19 -0500

    Add files via upload

--- noctalia.sh
+++ noctalia.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-echo "$HOME"
+
+USER_HOME="$HOME"
+echo "User home directory: $USER_HOME"
+
 # Path to your Python script
 PY_SCRIPT="$HOME/.config/hypr/noctalia/hyprConfToLua.py"
 
-# File you want to move
-SOURCE_FILE="$HOME/.config/hypr/noctalia/noctalia-colors.lua"
+# Input and output files
+INPUT_FILE="$HOME/.config/hypr/noctalia/noctalia-colors.conf"
+OUTPUT_FILE="$HOME/.config/hypr/noctalia/noctalia-colors.lua"
 
 # Destination directory
 DEST_DIR="$HOME/.config/hypr/"
 
 # 1. Execute Python script
-python hyprConfToLua.py noctalia-colors.conf -o noctalia-colors.lua
-
+python3 "$PY_SCRIPT" "$INPUT_FILE" -o "$OUTPUT_FILE"
 
 # 2. Check if Python ran successfully
 if [ $? -eq 0 ]; then
     echo "Python script executed successfully."
 
     # 3. Move the file
-    mv "$SOURCE_FILE" "$DEST_DIR"
+    mv "$OUTPUT_FILE" "$DEST_DIR"
 
     echo "File moved to $DEST_DIR"
 else
     echo "Python script failed. File not moved."
 fi
-
